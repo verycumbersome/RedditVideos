bdd0c954f567f0f64529e8723f1562aa38608f5d
verycumbersome <matthewsages@gmail.com>
2017-02-04 02:41:20 -0800

    Added Reddit Youtube url parser

---
+++ TopReddit.py
@@ -0,0 +1,22 @@
+import re
+try:
+    import praw
+except:
+    lib = "PRAW"
+    print "ImportError: %s is not found on your system" % (lib)
+    print "You must install %s to operate this program" % (lib)
+    sys.exit()
+
+r = praw.Reddit(user_agent="JustDudeStuff_V1",client_secret="AzvrzjEk6y6HL9MaEpKDpNHtbTo",client_id="8FzeKTyVy50rhQ")
+
+def GetTopSubmissions(subreddit, l):
+    SubredditInstance = r.subreddit(subreddit)
+    TopOfSub = SubredditInstance.top(limit = l)
+    for item in TopOfSub:
+        match = re.search(r"youtube\.com/.*v=([^&]*)", item.url)
+        if match:
+            result = match.group(1)
+            print result
+        else:
+            result = "no"
+GetTopSubmissions("Videos", 1000)
